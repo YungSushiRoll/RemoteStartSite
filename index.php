7311b8049e0eaad6f87cce61f923c61b45c30cf6
Joshua Gossett <j.gosset1@gmail.com>
2021-10-14 17:07:42 -0500

    section edits

--- index.php
+++ index.php
@@ -23,19 +23,32 @@
     </style>
 </head>
 <body style="background-color: lightgrey;">
-    <div class="container">
+    
         <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <p style="color: orange; display:inline">ROHRING</p><p style="color: palegreen; display:inline">STARTS</p>
-                    <img src="images/logo.png" alt="" style="width: auto; height: 25px; padding-left:.5em;" class="d-inline-block">                        
+                <a class="navbar-brand pb-0" href="#">
+                    <p style="color: orange; display:inline; font-size: 2em;">ROHRING</p><p style="color: palegreen; display:inline; font-size: 2em;">STARTS</p>
+                    <img src="images/logo.png" alt="" style="width: auto; height: 2em; padding-left:.5em;" class="d-inline-block mb-3">                        
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Systems
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#securitySystems">Security Systems</a></li>
+                            <li><a class="dropdown-item" href="#remoteStarters">Remote Starts</a></li>
+                            <li><a class="dropdown-item" href="#securityRemoteStart">Security and Remote Starts</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link">|</a>
+                    </li>
                     <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
@@ -45,12 +58,16 @@
                     <li class="nav-item">
                     <a class="nav-link" href="#remoteStarters">Remote Starts</a>
                     </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#securityRemoteStart">Security and Remote Starts</a>
+                    </li>
                 </ul>
                 </div>
             </div>
         </nav>
+    <div class="container">
         <!-- Security DIV -->
-        <div>
+        <div class="">
             <h1 class="text-left mt-3" id="securitySystems">Security Systems</h1>
             <div class="container rounded pt-3 mb-3 bg-white">
                 <div class="row">
@@ -61,21 +78,54 @@
                             <p style="font-family: 'Orbitron';">Python 3105P | 460 Max</p>
                         </fieldset>                
                     </div>
-                    <div class="col-9">                
-                        <h3 class="text-center" style="font-family: Orbitron;">1-Way Security System</h3>
-                        <div class="text-end" hidden>
-                            <button class="btn btn-secondary p-4 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#oneWayDetail" aria-expanded="false" aria-controls="twoWayDetail">
-                                Details
-                            </button>
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-center" style="font-size: 30px;">Some info about the Python 3105P</p>
-                        </div>
-                        <div class="collapse" id="oneWayDetail">
-                            <div class="card-body">
-                                <p style="font-size: 30px;">Some info about the Python 3105P</p>
+                    <div class="col-9">
+                        <div class="row">
+                            <h3 class="text-center" style="font-family: Orbitron; font-weight:bold;">1-Way Security System</h3>
+                            <div class="mt-3">
+                                <p style="font-size: 25px; font-family: Orbitron;">
+                                    An entry level security system that controls: lock/unlock, trunk pop, 
+                                    and monitors all door statuses for signs of entry.<br> 
+                                    Includes starter kill, shock sensor and a six tone siren.
+                                </p>
                             </div>
                         </div>
+                        <div class="row">
+                            <div>
+                                <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#oneWayDetail" aria-expanded="false" aria-controls="twoWayDetail">
+                                    KEY FEATURES AND SPECIFICATIONS &darr;
+                                </button>
+                            </div>
+                            <div class="collapse" id="oneWayDetail">
+                                <div class="card-body">
+                                    <ul>
+                                        <li>
+                                            Clone-Safe® Code-Hopping®
+                                        </li>
+                                        <li>
+                                            Stinger® DoubleGuard® Shock Sensor
+                                        </li>
+                                        <li>
+                                            Revenger® 6-tone Siren
+                                        </li>
+                                        <li>
+                                        2 Auxiliary Outputs
+                                        </li>
+                                        <li>
+                                        Security/keyless entry system
+                                        </li>
+                                        <li>
+                                        Up to 1/4 mile range*
+                                        </li>
+                                        <li>
+                                        Includes two 1-Way remotes
+                                        </li>
+                                        <li>
+                                        Directed® SmartStart Compatible**
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>                                        
                     </div>
                 </div>
             </div>
@@ -89,27 +139,62 @@
                         </fieldset>               
                     </div>
                     <div class="col-9">
-                        <h3 class="text-center" style="font-family: Orbitron;">2-Way Security System</h3>
-                        <div class="text-end" hidden>
-                            <button class="btn btn-secondary p-4" type="button" data-bs-toggle="collapse" data-bs-target="#twoWayDetail" aria-expanded="false" aria-controls="twoWayDetail">
-                                Details
-                            </button>
-                        </div>
-                        <div class="mt-3">
-                            <p class="text-center" style="font-size: 30px;">Some info about the Python 3305P</p>
-                        </div>
-                        <div class="collapse" id="twoWayDetail">
-                            <div class="card-body">
-                                <p style="font-size: 30px;">Some info about the Python 3305P</p>
+                        <div class="row">
+                            <h3 class="text-center" style="font-family: Orbitron; font-weight: bold;">2-Way Security System</h3>
+                            <div class="mt-3">
+                                <p style="font-size: 25px; font-family: Orbitron;">
+                                    A security system that controls: lock/unlock, trunk pop, 
+                                    and monitors all door statuses for signs of entry with improved range up to 2,000 feet away 
+                                    and 2 way remote that shows the current status of your vehicle.<br> 
+                                    Includes starter kill, shock sensor and a six tone siren.
+                                </p>
                             </div>
                         </div>
+                        
+                        <div class="row">
+                            <div>
+                                <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#twoWayDetail" aria-expanded="false" aria-controls="twoWayDetail">
+                                    KEY FEATURES AND SPECIFICATIONS &darr;
+                                </button>                                
+                            </div>
+                            <div class="collapse" id="twoWayDetail">
+                                <div class="card-body">
+                                    <ul>
+                                        <li>
+                                        Security and keyless entry system
+                                        </li>
+                                        <li>
+                                        Clone-Safe® Code-Hopping®                                        
+                                        </li>
+                                        <li>
+                                        Stinger® DoubleGuard® Shock Sensor                                    
+                                        </li>
+                                        <li>
+                                        Revenger® 6-tone Siren                                        
+                                        </li>
+                                        <li>
+                                        2 Auxiliary Outputs
+                                        </li>
+                                        <li>                                        
+                                        Up to 1/4 mile range*                                        
+                                        </li>
+                                        <li>
+                                        Includes 1-Way companion remote
+                                        </li>
+                                        <li>
+                                        Directed® SmartStart Compatible**
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>    
                     </div>
                 </div>
             </div>
         </div>
         <!-- Starters DIV -->
         <div>
-        <h1 class="text-left" id="remoteStarters">Remote Starters</h1>
+            <h1 class="text-left" id="remoteStarters">Remote Starters</h1>
             <div class="container rounded pt-3 mb-3 bg-white">
                 <div class="row">
                     <div class="col-3 text-center">
@@ -120,15 +205,16 @@
                         </fieldset>                
                     </div>
                     <div class="col-9">                
-                        <h3 class="text-center" style="font-family: Orbitron;">1-Way Remote Start System</h3>
-                        <div class="text-end" hidden>
-                            <button class="btn btn-secondary p-4 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#oneWayDetail" aria-expanded="false" aria-controls="twoWayDetail">
-                                Details
-                            </button>
-                        </div>
+                        <h3 class="text-center" style="font-family: Orbitron; font-weight:bold;">1-Way Remote Start System</h3>
                         <div class="mt-3">
-                            <p class="text-center" style="font-size: 30px;">Some info about the Python 4105P</p>
+                            <p style="font-size: 30px;">  
+                                1-Way Remote car starting system with trunk pop, lock on lock features.*
+                            </p>
+                            <p style="font-size: 15px;">
+                            *** One way remote starters do not communicate back to the individual starting the vehicle. ***
+                            </p>
                         </div>
+                        <!-- Continue to edit detail stuff -->
                         <div class="collapse" id="oneWayDetail">
                             <div class="card-body">
                                 <p style="font-size: 30px;">Some info about the Python 4105P</p>
@@ -159,6 +245,37 @@
                         <div class="collapse" id="twoWayDetail">
                             <div class="card-body">
                                 <p style="font-size: 30px;">Some info about the Python 4806P</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Secuirty & Starters DIV -->
+        <div>
+            <h1 class="text-left" id="securityRemoteStart">Security with Remote Starter</h1>
+            <div class="container rounded pt-3 mb-3 bg-white">
+                <div class="row">
+                    <div class="col-3 text-center">
+                        <fieldset class="border border-warning mb-3 form-group rounded shadow-lg">
+                            <legend class="text-warning" style="font-family: Orbitron; font-weight:bold">Best</legend>                
+                            <img class="img-fluid" src="images/5706P.png" alt="Python 5706P">
+                            <p style="font-family: 'Orbitron';">Python 5706P</p>
+                        </fieldset>                
+                    </div>
+                    <div class="col-9">                
+                        <h3 class="text-center" style="font-family: Orbitron;">LCD 2-Way Security and Remote Start System</h3>
+                        <div class="text-end" hidden>
+                            <button class="btn btn-secondary p-4 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#oneWayDetail" aria-expanded="false" aria-controls="twoWayDetail">
+                                Details
+                            </button>
+                        </div>
+                        <div class="mt-3">
+                            <p class="text-center" style="font-size: 30px;">Some info about the Python 5706P</p>
+                        </div>
+                        <div class="collapse" id="oneWayDetail">
+                            <div class="card-body">
+                                <p style="font-size: 30px;">Some info about the Python 5706P</p>
                             </div>
                         </div>
                     </div>
